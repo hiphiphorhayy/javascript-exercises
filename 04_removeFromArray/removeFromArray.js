766c3dea54042b98d712680e10ac7a2041abbd1e
George Montoya <94158336+hiphiphorhayy@users.noreply.github.com>
2023-05-03 00:29:20 -0400

    Completed JS exercises lesson

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...theArgs) {
+    for (let i = 0; i < theArgs.length; i++) {
+        if (array.includes(theArgs[i])) {
+            array.splice(array.indexOf(theArgs[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
